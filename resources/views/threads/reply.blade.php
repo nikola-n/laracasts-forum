53f4ae5fe2cdb0078415300399f43d0958b8b464
Nikola <nikola@thecodeconnectors.nl>
2020-06-14 13:09:01 +0200

    add reply form

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,10 +1,9 @@
-<div class="card">
+<div class="card mb-2">
     <div class="card-body">
         <div class="card-header">
             <a href="#">
                 {{ $reply->owner->name }}
             </a> said {{ $reply->created_at->diffForHumans() }}...
-
         </div>
         {{ $reply->body }}
     </div>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mb-4">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
@@ -22,5 +22,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form method="POST" action="{{ route('replies.store', $thread->id) }}">
+                        @csrf
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
