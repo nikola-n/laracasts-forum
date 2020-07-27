470c3211d4f64bc0b6a881dedd932f11ca1876b6
Nikola <nikola@thecodeconnectors.nl>
2020-07-27 19:35:30 +0200

    thread subscriptions part 1

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -57,4 +57,29 @@ class ThreadTest extends TestCase
 
         $this->assertInstanceOf(Channel::class, $thread->channel);
     }
+
+    /** @test */
+    public function a_thread_can_be_subscribed_to()
+    {
+        //we have a thread
+        $thread = create('App\Thread');
+        //and auth user
+        //$this->signIn();
+        //when the user subscribes to the thread
+        $thread->subscribe($userId = 1);
+        //then we should be able to fetch all threads that the user has subscribed to.
+       $this->assertEquals(1, $thread->subscriptions()->where('user_id', $userId)->count());
+    }
+
+    /** @test */
+    public function a_thread_can_be_unsubscribed_from()
+    {
+        $thread = create('App\Thread');
+
+        $thread->subscribe($userId = 1);
+
+        $thread->unsubscribe($userId);
+
+        $this->assertCount(0,$thread->subscriptions);
+    }
 }
